b3c3d885493cef5624e3631656cae2ac574df587
Kopano <137108091+KopanoModika@users.noreply.github.com>
2025-01-29 18:54:09 +0200

    statements to execute when input is recived from the user

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function getWeather(){
     const apiKey = '7d1e08b73fbc9f5dfa5719c3dcdca392';
     const city = document.getElementById('city').value;
+
+    if(!city){
+        alert('Please enter a city');
+        return;
+    }
+
+    const currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={7d1e08b73fbc9f5dfa5719c3dcdca392}';
+    const forcastUrl = 'https://api.openweathermap.org/data/2.5/forcast?q=${city}&appid={7d1e08b73fbc9f5dfa5719c3dcdca392}';
+
+    fetch(currentWeatherUrl)
+    .then(response => response.json())
+    .then(data => {
+        console.error('Error fetching current weather data', error);
+        alert('Error fetching current weather data. Please try again');
+    });
 }
